7a80481477be3cd42709b1a267f453b39b2abd8c
Mystique <mystique@quantumfoam.uni.cc>
2019-02-17 13:06:28 +0100

    Adding Ownership

---
+++ __about__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "Predict Attire for Weather"
+__summary__ = ("Based on the weather for the day, recommend the attire for a comfortable day.")
+__uri__ = "https://github.com/miztiik"
+
+__version__ = "0.0.1"
+
+__author__ = "Mystique"
+__email__ = "@github.com"
+__license__ = "Contact Me"
+__copyright__ = "Copyright 2019 {0}".format(__author__)
