d37d456a1b8197aaf5a3f92d921db911105d720d
Mystique <mystique@quantumfoam.uni.cc>
2019-02-17 22:11:41 +0100

    Helper script to build, test dockerfile and images

---
+++ helper_scripts/build_test_docker_image.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+set -e
+
+# Prepare dev/container environment for building image
+cd /var
+
+## Install git client
+yum -y install git
+
+## Lets get the application code
+git clone https://github.com/miztiik/predict-attire-for-weather.git && \
+cd /var/predict-attire-for-weather
+
+## Build the container image
+
+### Build
+docker build --tag="predict-attire-for-weather" .
+
+### Test the new image
+docker run -dti -p 80:80 --name attire_recommender predict-attire-for-weather
+
+### Clean Up
+docker stop attire_recommender;docker rm attire_recommender; docker rmi predict-attire-for-weather
